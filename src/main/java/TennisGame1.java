811931ba7e9c2aa6c8e6a29bfd083407939e2d24
jcelisca <celiss1997@gmail.com>
2022-03-06 16:35:10 -0500

    Refactorización de Game1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -18,9 +18,88 @@ public class TennisGame1 implements TennisGame {
             m_score2 += 1;
     }
 
-    public String getScore() {
+
+    public String changeScore(int tempScore, String score){
+        switch (tempScore)
+            {
+                case 0:
+                    score = "Love";
+                    break;
+                case 1:
+                    score = "Fifteen";
+                    break;
+                case 2:
+                    score = "Thirty";
+                    break;
+                case 3:
+                    score = "Forty";
+                    break;
+                default:
+                    score ="";
+            }
+        return score;
+    }
+
+    public boolean deuceScore(){
+        if(m_score1 == m_score2 && (m_score1 >=3)){
+            return true;
+        }
+        return false;
+    }
+
+    public String highScore(){
+        if(m_score1 > m_score2){
+            return player1Name;
+        }
+        return player2Name;
+    }
+
+    public boolean advantageScore(){
+        if(m_score1 >=4 && m_score1 >= (m_score2 + 1)){
+            return true;
+        }
+        if(m_score2 >=4 && m_score2 >= (m_score1 + 1)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean winScore(){
+        if(m_score1 >=4 && m_score1 >= (m_score2 + 2)){
+            return true;
+        }
+        if(m_score2 >=4 && m_score2 >= (m_score1 + 2)){
+            return true;
+        }
+        return false;
+    }
+
+    public String getScore(){
+
+        if(deuceScore()){
+            return "Deuce";
+        }
+
+        if(winScore()){
+            return "Win for "+highScore();
+        }
+
+        if(advantageScore()){
+            return "Advantage "+ highScore();
+        }
+
+        if(m_score1 == m_score2){
+            return changeScore(m_score1, player1Name) + "-All";
+        }
+
+        return changeScore(m_score1, player1Name) +"-"+ changeScore(m_score2, player2Name);
+    }
+
+
+    /*public String getScore() {
         String score = "";
         int tempScore=0;
+
         if (m_score1==m_score2)
         {
             switch (m_score1)
@@ -37,7 +116,6 @@ public class TennisGame1 implements TennisGame {
                 default:
                         score = "Deuce";
                     break;
-                
             }
         }
         else if (m_score1>=4 || m_score2>=4)
@@ -72,5 +150,5 @@ public class TennisGame1 implements TennisGame {
             }
         }
         return score;
-    }
+    }*/
 }
