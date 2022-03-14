60934c4201defda9cd18e937bb23d89006baf389
jcelisca <celiss1997@gmail.com>
2022-03-14 10:15:58 -0500

    pequeños cambios

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -69,27 +69,9 @@ public class TennisGame2 implements TennisGame {
         }
 
         if (P1point > P2point && P1point < 4) {
-            /*if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;*/
             score = big(P1point,P2point);
         }
         if (P2point > P1point && P2point < 4) {
-            /*if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;*/
             score = big(P2point,P1point);
         }
 
